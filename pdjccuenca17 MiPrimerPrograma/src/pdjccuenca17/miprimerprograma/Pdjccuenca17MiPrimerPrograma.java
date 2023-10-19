92251381872cb505ea11c8ce14b836df6f9cadf3
Jean <jccuenca@utpl.edu.ec>
2023-10-19 17:12:51 -0500

    Este es mi primer programa que subo de la clase

---
+++ pdjccuenca17 MiPrimerPrograma/src/pdjccuenca17/miprimerprograma/Pdjccuenca17MiPrimerPrograma.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdjccuenca17.miprimerprograma;
+
+/**
+ *
+ * @author SALAI
+ */
+public class Pdjccuenca17MiPrimerPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    }
+    
+}
